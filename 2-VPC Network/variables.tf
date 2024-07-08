31e3278f28c141183783d86e0e5a496ff5512c20
BrunoFerreira10 <brunoferreira86dev@gmail.com>
2024-07-08 18:14:32 -0300

    Fix

--- 2-VPC Network/variables.tf
+++ 2-VPC Network/variables.tf
@@ -1,24 +1,29 @@
 variable "projeto" {
   type        = string
   description = "Descrição do projeto."
+  default = data.terraform_remote_state.remote-state-information.outputs.projeto
 }
 
 variable "cliente" {
   type        = string
   description = "Cliente do projeto."
+  default = data.terraform_remote_state.remote-state-information.outputs.cliente
 }
 
 variable "autor" {
   type        = string
   description = "Autor de edição."
+  default = data.terraform_remote_state.remote-state-information.outputs.autor
 }
 
 variable "regiao" {
   type        = string
   description = "Região onde a infraestrutura será criada."
+  default = data.terraform_remote_state.remote-state-information.outputs.regiao
 }
 
 variable "remote-state-bucket" {
   type        = string
   description = "Bucket name onde está o remote state"
+  default = data.terraform_remote_state.remote-state-information.outputs.remote-state-bucket-name
 }
