9c36d6452beda36012bc5327a6eff4d9fc9c8cac
BrunoFerreira10 <brunoferreira86dev@gmail.com>
2024-07-08 20:45:41 -0300

    Fix 10

--- 4a-EC2 Setup/variables.tf
+++ 4a-EC2 Setup/variables.tf
@@ -7,8 +7,3 @@ variable "remote-state-bucket" {
   type        = string
   description = "Bucket name onde está o remote state"
 }
-
-variable "AWS_SSH_PRIVATE_KEY" {
-  description = "Chave privada SSH para conexão"
-  type        = string
-}
