d6262170ad0c6a1f2dd0ee82bef19d94e2f1e8f4
BrunoFerreira10 <brunoferreira86dev@gmail.com>
2024-07-08 19:16:29 -0300

    Fix 10

---
+++ 3a-Bastion Host/variables.tf
@@ -0,0 +1,15 @@
+variable "regiao" {
+  type        = string
+  description = "Região onde a infraestrutura será criada."
+}
+
+variable "remote-state-bucket" {
+  type        = string
+  description = "Bucket name onde está o remote state"
+}
+
+variable "AWS_SSH_PRIVATE_KEY" {
+  description = "Chave privada SSH para conexão"
+  type        = string  
+  sensitive = true
+}
