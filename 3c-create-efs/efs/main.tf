dc209ef644634bcccdd9298ebb27115ce8c9e65a
BrunoFerreira10 <brunoferreira86dev@gmail.com>
2024-07-08 20:10:40 -0300

    Fix 10

--- 3c-create-efs/efs/main.tf
+++
@@ -1,8 +0,0 @@
-data "terraform_remote_state" "remote-state-vpc" {
-  backend = "s3"
-  config = {
-    region = "us-east-1"
-    bucket = "remote-state-odoo"
-    key    = "create-network/terraform.tfstate"
-  }
-}
