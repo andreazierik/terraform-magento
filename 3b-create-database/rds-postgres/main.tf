831270e00ae023cef5a249e542a1c83c1ab54493
BrunoFerreira10 <brunoferreira86dev@gmail.com>
2024-07-08 19:55:16 -0300

    Fix 10

--- 3b-create-database/rds-postgres/main.tf
+++ 3b-create-database/rds-postgres/main.tf
@@ -1,8 +1,8 @@
 data "terraform_remote_state" "remote-state-vpc" {
   backend = "s3"
   config = {
-    region = "us-east-1"
-    bucket = "remote-state-odoo"
-    key    = "create-network/terraform.tfstate"
+    region = var.regiao
+    bucket = var.remote-state-bucket
+    key    = "VPC Network/terraform.tfstate"
   }
 }
