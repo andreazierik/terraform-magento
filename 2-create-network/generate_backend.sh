117bc48cfeb177235f5c529fb5386a4cbe21b808
BrunoFerreira10 <brunoferreira86dev@gmail.com>
2024-07-08 14:21:22 -0300

    Fix

--- 2-create-network/generate_backend.sh
+++ 2-create-network/generate_backend.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cat <<EOF > ./backend.tf
-region = ${REGION}
-bucket = ${REMOTE_STATE_BUCKET}
-key    = ${KEY}
+region = "${REGION}"
+bucket = "${REMOTE_STATE_BUCKET}"
+key    = "${KEY}"
 EOF
