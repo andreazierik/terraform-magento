e918a5597cbaa983686f3c180c4a4b20441c378d
BrunoFerreira10 <brunoferreira86dev@gmail.com>
2024-07-08 16:51:01 -0300

    Fix

--- 1-Remote State/generate_tfvars.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cat <<EOF > "./terraform.tfvars"
-regiao                = "${REGION}"
-remote-state-bucket   = "${REMOTE_STATE_BUCKET}"
-EOF
--- 1-Remote State/variables.tf
+++ 1-Remote State/variables.tf
@@ -1,24 +1,21 @@
 variable "projeto" {
   type        = string
   description = "Descrição do projeto."
-  default     = "oficina-19-magento"
 }
 
 variable "cliente" {
   type        = string
   description = "Cliente do projeto."
-  default     = "Cloud Treinamentos"
 }
 
 variable "autor" {
   type        = string
   description = "Autor de edição."
-  default     = "Bruno Ferreira"
 }
 
 variable "regiao" {
   type        = string
-  description = "Região onde a infraestrutura será criada."  
+  description = "Região onde a infraestrutura será criada."
 }
 
 variable "remote-state-bucket" {
