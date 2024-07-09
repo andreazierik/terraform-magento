72b1ad6ccc05c656db8419556d8c1d033fd4ed2a
BrunoFerreira10 <brunoferreira86dev@gmail.com>
2024-07-08 23:20:24 -0300

    Fix 10

--- 3b-RDS Database/outputs.tf
+++ 3b-RDS Database/outputs.tf
@@ -1,9 +1,9 @@
-output "rds-postgres-rds-1-id" {
+output "rds-database-rds-1-id" {
   description = "ID do rds-1"
-  value       = module.rds-postgres.rds-1-id
+  value       = module.rds-database.rds-1-id
 }
 
-output "rds-postgres-rds-1-endpoint" {
+output "rds-database-rds-1-endpoint" {
   description = "Endpoint do rds-1"
-  value       = module.rds-postgres.rds-1-endpoint
+  value       = module.rds-database.rds-1-endpoint
 }
