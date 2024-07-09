5c4fdaeec9832404c56f8c9f0a0424dbc1605ff0
BrunoFerreira10 <brunoferreira86dev@gmail.com>
2024-07-09 16:19:34 -0300

    Fix 10

--- 4b-AMI Create/ami/ami.tf
+++ 4b-AMI Create/ami/ami.tf
@@ -1,7 +1,7 @@
 // Criar AMI
 resource "aws_ami_from_instance" "ec2-ami" {
   name                    = "terraform-ami-magento-v1"
-  source_instance_id      = data.terraform_remote_state.remote-state-base-ec2.outputs.ec2-base-ami
+  source_instance_id      = data.terraform_remote_state.remote-state-base-ec2.outputs.ec2-base-ami-id
   snapshot_without_reboot = true
 
   tags = {
