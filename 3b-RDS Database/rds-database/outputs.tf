3e459d00039ca384ff04540033d854e43b02bd12
BrunoFerreira10 <brunoferreira86dev@gmail.com>
2024-07-08 23:18:21 -0300

    Fix 10

---
+++ 3b-RDS Database/rds-database/outputs.tf
@@ -0,0 +1,9 @@
+output "rds-1-id" {
+  description = "ID do rds-1"
+  value       = aws_db_instance.rds-1.id
+}
+
+output "rds-1-endpoint" {
+  description = "ID do rds-1"
+  value       = aws_db_instance.rds-1.endpoint
+}
---
+++ 3b-RDS Database/rds-database/rds.tf
@@ -0,0 +1,29 @@
+resource "aws_db_subnet_group" "rds-1-subnet-group" {
+  name = "rds-1-subnet-group"
+  subnet_ids = [
+    data.terraform_remote_state.remote-state-vpc.outputs.vpcs-vpc-1-subnet-private-1a-id,
+    data.terraform_remote_state.remote-state-vpc.outputs.vpcs-vpc-1-subnet-private-1b-id
+  ]
+
+  tags = {
+    Name = "My DB subnet group"
+  }
+}
+
+resource "aws_db_instance" "rds-1" {
+  allocated_storage    = 20
+  db_name              = "magentodb"
+  engine               = "mysql"
+  engine_version       = "8.0"
+  instance_class       = "db.t3.micro"
+  username             = "magentoadmin"
+  password             = "123456789"
+  parameter_group_name = "default.mysql8.0"
+  db_subnet_group_name = aws_db_subnet_group.rds-1-subnet-group.name
+  skip_final_snapshot  = true
+  multi_az             = false
+
+  vpc_security_group_ids = [
+    data.terraform_remote_state.remote-state-vpc.outputs.vpcs-vpc-1-sg-rds-id
+  ]
+}
