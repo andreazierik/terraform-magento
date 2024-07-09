58533a4fef8fc8137b9271ef6fe1e078943e01fb
BrunoFerreira10 <brunoferreira86dev@gmail.com>
2024-07-08 21:22:12 -0300

    Fix 10

--- 4a-EC2 Setup/ec2-setup/outputs.tf
+++ 4a-EC2 Setup/ec2-setup/outputs.tf
@@ -1,3 +1,7 @@
 output "vm-1-id" {
   value = aws_instance.vm-1.id
 }
+
+output "vm-1-public-ip" {
+  value = aws_instance.vm-1.public_ip
+}
--- 4a-EC2 Setup/outputs.tf
+++ 4a-EC2 Setup/outputs.tf
@@ -1,4 +1,9 @@
-output "ec2-base-ami" {
-  description = "EC2 base para criar a AMI"
+output "ec2-base-ami-id" {
+  description = "ID da EC2 base para criar a AMI"
   value       = module.ec2-setup.vm-1-id
+}
+
+output "ec2-base-ami-public-ip" {
+  description = "IP público da EC2 base para criar a AMI"
+  value       = module.ec2-setup.vm-1-public-ip
 }
