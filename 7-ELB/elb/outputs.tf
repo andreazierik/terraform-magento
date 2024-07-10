d767ca53f1d6c608eaf5de8992a26a11037116cb
BrunoFerreira10 <brunoferreira86dev@gmail.com>
2024-07-09 23:10:36 -0300

    Fix 10

--- 7-ELB/elb/outputs.tf
+++ 7-ELB/elb/outputs.tf
@@ -10,6 +10,6 @@ output "alb-1-dns-name" {
   value = aws_lb.alb-1.dns_name
 }
 
-output "alb-1-id" {
+output "alb-1-zone-id" {
   value = aws_lb.alb-1.zone_id
 }
--- 7-ELB/outputs.tf
+++ 7-ELB/outputs.tf
@@ -7,9 +7,9 @@ output "elb-alb-1-name" {
 }
 
 output "elb-alb-1-dns-name" {
-  value = module.elb.alb-odoo-1-dns-name
+  value = module.elb.alb-1-dns-name
 }
 
 output "elb-alb-1-zone-id" {
-  value = module.elb.alb-odoo-1-zone-id
+  value = module.elb.alb-1-zone-id
 }
