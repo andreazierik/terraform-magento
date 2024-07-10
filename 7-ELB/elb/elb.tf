9f4e09b204079740e6377bf270f31a4ce0ba42ca
BrunoFerreira10 <brunoferreira86dev@gmail.com>
2024-07-10 10:18:58 -0300

    Fix 10

--- 7-ELB/elb/elb.tf
+++ 7-ELB/elb/elb.tf
@@ -112,7 +112,7 @@ resource "aws_autoscaling_group" "asg-alb-1" {
 
   // Group Details
   capacity_rebalance = true
-  desired_capacity   = 2
+  desired_capacity   = 1
   max_size           = 4
   min_size           = 1
 
