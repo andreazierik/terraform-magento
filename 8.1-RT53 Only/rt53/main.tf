a9279ea4fbba7f6e62c32d340fa8837b83283294
BrunoFerreira10 <brunoferreira86dev@gmail.com>
2024-07-09 23:40:35 -0300

    Fix 10

--- 8.1-RT53 Only/rt53/main.tf
+++ 8.1-RT53 Only/rt53/main.tf
@@ -16,11 +16,11 @@ data "terraform_remote_state" "remote-state-base-ec2" {
   }
 }
 
-# data "terraform_remote_state" "remote-computing" {
-#   backend = "s3"
-#   config = {
-#     region = var.regiao
-#     bucket = var.remote-state-bucket
-#     key    = "create-computing/terraform.tfstate"
-#   }
-# }
+data "terraform_remote_state" "remote-computing" {
+  backend = "s3"
+  config = {
+    region = var.regiao
+    bucket = var.remote-state-bucket
+    key    = "create-computing/terraform.tfstate"
+  }
+}
