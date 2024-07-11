06cde31ab12cc4cf76ad86d4d0291e6abac04b12
BrunoFerreira10 <brunoferreira86dev@gmail.com>
2024-07-11 14:16:41 -0300

    Fix 10

--- 7-ELB/elb/elb.tf
+++ 7-ELB/elb/elb.tf
@@ -51,9 +51,18 @@ resource "aws_lb_listener" "listener-http-alb-1" {
   port              = "80"
   protocol          = "HTTP"
 
-  default_action {    
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.tgrp-alb-1.arn  
+  # default_action {    
+  #   type             = "forward"
+  #   target_group_arn = aws_lb_target_group.tgrp-alb-1.arn  
+  # }
+  default_action {
+    type = "redirect"
+
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_302"
+    }
   }
 }
 
