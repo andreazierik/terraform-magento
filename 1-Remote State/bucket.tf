cb9eba826a3b8574a8921d2e59bc359334ec0bc5
BrunoFerreira10 <brunoferreira86dev@gmail.com>
2024-07-11 15:41:30 -0300

    Fix 10

--- 1-Remote State/bucket.tf
+++ 1-Remote State/bucket.tf
@@ -1,39 +1,11 @@
-# Caso o remote state já exista, para atualização dos outputs.
-data "terraform_remote_state" "bucket-existente" {
-  backend = "s3"
-  config = {
-    region = var.regiao
-    bucket = var.remote-state-bucket
-    key    = "Remote State/terraform.tfstate"
-  }
-}
-
-# Caso ainda não exista.
 resource "aws_s3_bucket" "s3-remote-state" {
-  bucket = var.remote-state-bucket
-  
-  lifecycle {
-    prevent_destroy = true
-    ignore_changes = [
-      bucket
-    ]
-  }
+  bucket = var.remote-state-bucket  
 }
 
 resource "aws_s3_bucket_versioning" "versionamento-s3-remote-state" {
-  bucket = try(
-    aws_s3_bucket.s3-remote-state.id, 
-    data.terraform_remote_state.bucket-existente.outputs.remote_state_bucket_name
-  )
-
+  bucket = aws_s3_bucket.s3-remote-state.id
+  
   versioning_configuration {
     status = "Enabled"
-  }
-
-  lifecycle {
-    ignore_changes = [
-      bucket,
-      versioning_configuration
-    ]
-  }
+  }  
 }
