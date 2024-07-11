8a799e5d8d64f5e54a20ba1acc03cf99a801f5a6
BrunoFerreira10 <brunoferreira86dev@gmail.com>
2024-07-11 16:45:10 -0300

    Fix 10

---
+++ 2-VPC Network/outputs-vpc-1.tf
@@ -0,0 +1,75 @@
+# VPC 1 Information
+output "vpcs-vpc-1-name" {
+  description = "Nome da VPC-1"
+  value       = module.vpcs.vpc-1-name
+}
+
+output "vpcs-vpc-1-id" {
+  description = "ID da VPC-1"
+  value       = module.vpcs.vpc-1-id
+}
+
+output "vpcs-subnet-vpc-1-public-1a-id" {
+  description = "VPC-1: ID da subnet publica 1a"
+  value       = module.vpcs.subnet-vpc-1-public-1a-id
+}
+
+output "vpcs-subnet-vpc-1-public-1b-id" {
+  description = "VPC-1: ID da subnet publica 1b"
+  value       = module.vpcs.subnet-vpc-1-public-1b-id
+}
+
+output "vpcs-subnet-vpc-1-private-1a-id" {
+  description = "VPC-1: ID da subnet privada 1a"
+  value       = module.vpcs.subnet-vpc-1-private-1a-id
+}
+
+output "vpcs-subnet-vpc-1-private-1b-id" {
+  description = "VPC-1: ID da subnet privada 1b"
+  value       = module.vpcs.subnet-vpc-1-private-1b-id
+}
+
+output "sg-vpc-1-allow-all-name" {
+  description = "VPC-1: Security group tudo liberado Name"
+  value       = module.vpcs.sg-vpc-1-allow-all.name
+}
+
+output "sg-vpc-1-alb-1-tgrp-1-id" {
+  description = "VPC-1: Security group para instancias no Target group1 do ALB 1, ID"
+  value       = module.vpcs.sg-vpc-1-alb-1-tgrp-1.id
+}
+
+output "sg-vpc-1-alb-1-tgrp-1-name" {
+  description = "VPC-1: Security group para instancias no Target group 1 do ALB 1, Name"
+  value       = module.vpcs.sg-vpc-1-alb-1-tgrp-1.name
+}
+
+output "sg-vpc-1-rds-1-id" {
+  description = "VPC-1: Security group para RDS 1, ID"
+  value       = module.vpcs.sg-vpc-1-rds-1.id
+}
+
+output "sg-vpc-1-rds-1-name" {
+  description = "VPC-1: Security group para RDS 1, Name"
+  value       = module.vpcs.sg-vpc-1-rds-1.name
+}
+
+output "sg-vpc-1-efs-1-mountpoints-id" {
+  description = "VPC-1: Security group para EFS 1 mount points, ID"
+  value       = module.vpcs.sg-vpc-1-efs-1-mountpoints.id
+}
+
+output "sg-vpc-1-efs-1-mountpoints-name" {
+  description = "VPC-1: Security group para EFS 1 mount points, Name"
+  value       = module.vpcs.sg-vpc-1-efs-1-mountpoints.name
+}
+
+output "sg-vpc-1-alb-1-id" {
+  description = "VPC-1: Security group para ALB 1, ID"
+  value       = module.vpcs.sg-vpc-1-alb-1.id
+}
+
+output "sg-vpc-1-alb-1-name" {
+  description = "VPC-1: Security group para ALB 1, Name"
+  value       = module.vpcs.sg-vpc-1-alb-1.name
+}
