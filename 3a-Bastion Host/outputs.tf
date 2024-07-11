16cd88f2d618f8e2fd62893e09cf9ea9e4a598cd
BrunoFerreira10 <brunoferreira86dev@gmail.com>
2024-07-11 17:24:24 -0300

    Fix 10

--- 3a-Bastion Host/outputs.tf
+++ 3a-Bastion Host/outputs.tf
@@ -1,9 +1,9 @@
-output "bastion-vpc-1-id" {
+output "ec2-bastion-bastion-vpc-1-id" {
   description = "VPC-1: Bastion Host, ID"
   value       = module.ec2-bastion.bastion-vpc-1-id
 }
 
-output "bastion-vpc-1-public_ip" {
+output "ec2-bastion-bastion-vpc-1-public_ip" {
   description = "VPC-1: Bastion Host, Public IP"
   value       = module.ec2-bastion.bastion-vpc-1-public-ip
 }
