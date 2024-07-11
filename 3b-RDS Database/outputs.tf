8583f1db5a8dfe620f117bc112a0a354f54e0f6d
BrunoFerreira10 <brunoferreira86dev@gmail.com>
2024-07-11 18:49:51 -0300

    Fix 10

--- 3b-RDS Database/outputs.tf
+++ 3b-RDS Database/outputs.tf
@@ -1,9 +1,9 @@
 output "rds-rds-1-id" {
   description = "ID do rds-1"
-  value       = module.rds-database.rds-1-id
+  value       = module.rds.rds-1-id
 }
 
 output "rds-rds-1-endpoint" {
   description = "Endpoint do rds-1"
-  value       = module.rds-database.rds-1-endpoint
+  value       = module.rds.rds-1-endpoint
 }
