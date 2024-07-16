92c0b14e429f6f472e83a2340897238438ce442a
BrunoFerreira10 <brunoferreira86dev@gmail.com>
2024-07-16 13:36:03 -0300

    Fix

--- 3a-Bastion Host/ec2-bastion/ec2.tf
+++ 3a-Bastion Host/ec2-bastion/ec2.tf
@@ -2,8 +2,10 @@ resource "aws_instance" "bastion-vpc-1" {
   # Ubuntu Server 24.04 LTS (HVM), SSD Volume Type
   ami = "ami-04b70fa74e45c3917"
 
-  instance_type = "t3a.large"
-  key_name      = "aws-dev-console-admin"
+  #instance_type = "t3a.large"
+  instance_type = "t3.micro"
+  #key_name      = "aws-dev-console-admin"
+  key_name      = "aws-services-ec2-ssh"
   subnet_id     = data.terraform_remote_state.remote-state-vpc.outputs.vpcs-subnet-vpc-1-public-1a-id
 
   vpc_security_group_ids = [
