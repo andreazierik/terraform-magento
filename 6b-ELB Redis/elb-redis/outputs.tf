45ebdcc0e57dd1dfa8d1a6db11eb7eacabaf3dff
BrunoFerreira10 <brunoferreira86dev@gmail.com>
2024-07-17 22:20:16 -0300

    Fix

--- 6b-ELB Redis/elb-redis/outputs.tf
+++ 6b-ELB Redis/elb-redis/outputs.tf
@@ -1,15 +1,15 @@
-output "alb-1-id" {
-  value = aws_lb.alb-1.id
+output "nlb-1-id" {
+  value = aws_lb.nlb-1.id
 }
 
-output "alb-1-name" {
-  value = aws_lb.alb-1.name
+output "nlb-1-name" {
+  value = aws_lb.nlb-1.name
 }
 
-output "alb-1-dns-name" {
-  value = aws_lb.alb-1.dns_name
+output "nlb-1-dns-name" {
+  value = aws_lb.nlb-1.dns_name
 }
 
-output "alb-1-zone-id" {
-  value = aws_lb.alb-1.zone_id
+output "nlb-1-zone-id" {
+  value = aws_lb.nlb-1.zone_id
 }
