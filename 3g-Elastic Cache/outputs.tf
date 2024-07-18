df8f770b58260976d7ca670e8df225d0fdd0a8e2
BrunoFerreira10 <brunoferreira86dev@gmail.com>
2024-07-18 13:25:40 -0300

    Fix

--- 3g-Elastic Cache/outputs.tf
+++ 3g-Elastic Cache/outputs.tf
@@ -1,6 +1,6 @@
-output "elasticcache-elasticcache-1-cluster-id" {
-  value = module.elasticcache.elasticcache-1-cluster-id
-}
+# output "elasticcache-elasticcache-1-cluster-id" {
+#   value = module.elasticcache.elasticcache-1-cluster-id
+# }
 
 output "elasticcache-elasticcache-1-cluster-address" {
   value = module.elasticcache.elasticcache-1-cluster-address
