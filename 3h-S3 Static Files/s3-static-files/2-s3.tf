dfe9b993c936771f11d6e89f2694950493b2bc3f
BrunoFerreira10 <brunoferreira86dev@gmail.com>
2024-07-18 16:16:29 -0300

    Fix

--- 3h-S3 Static Files/s3-static-files/2-s3.tf
+++ 3h-S3 Static Files/s3-static-files/2-s3.tf
@@ -6,10 +6,18 @@ resource "aws_s3_bucket" "magento-static-files" {
   }
 }
 
-resource "aws_s3_bucket_acl" "magento-static-files-acl" {
-  bucket = aws_s3_bucket.magento-static-files.id
-  acl    = "private"
-}
+# resource "aws_s3_bucket_versioning" "versionamento-s3-remote-state" {
+#   bucket = aws_s3_bucket.magento-static-files.id
+
+#   versioning_configuration {
+#     status = "Enabled"
+#   }
+# }
+
+# resource "aws_s3_bucket_acl" "magento-static-files-acl" {
+#   bucket = aws_s3_bucket.magento-static-files.id
+#   acl    = "private"
+# }
 
 resource "aws_s3_bucket_public_access_block" "magento-static-files" {
   bucket = aws_s3_bucket.magento-static-files.bucket
