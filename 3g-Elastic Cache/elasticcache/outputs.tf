420543dd6adc7921987b3ea3ab5713dfc6a7781d
BrunoFerreira10 <brunoferreira86dev@gmail.com>
2024-07-18 14:10:42 -0300

    Fix

--- 3g-Elastic Cache/elasticcache/outputs.tf
+++ 3g-Elastic Cache/elasticcache/outputs.tf
@@ -1,11 +1,11 @@
-output "elasticcache-1-cluster-id" {
-  value = aws_elasticache_cluster.elasticcache-1.cluster_id
-}
-
 output "elasticcache-1-cluster-address" {
   value = aws_elasticache_cluster.elasticcache-1.cluster_address
 }
 
 output "elasticcache-1-configuration-endpoint" {
   value = aws_elasticache_cluster.elasticcache-1.configuration_endpoint
+}
+
+output "elasticcache-1-cluster-id" {
+  value = aws_elasticache_cluster.elasticcache-1.cluster_id
 }
--- 3g-Elastic Cache/outputs.tf
+++ 3g-Elastic Cache/outputs.tf
@@ -1,11 +1,13 @@
-output "elasticcache-elasticcache-1-cluster-id" {
-  value = module.elasticcache.elasticcache-1-cluster-id
-}
-
-output "elasticcache-1-cluster-address" {
+output "elasticcache-elasticcache-1-cluster-address" {
+  description = "value"
   value = module.elasticcache.elasticcache-1-cluster-address
 }
 
-output "elasticcache-1-configuration-endpoint" {
+output "elasticcache-elasticcache-1-configuration-endpoint" {
+  description = "value"
   value = module.elasticcache.elasticcache-1-configuration-endpoint
+}
+
+output "elasticcache-elasticcache-1-cluster-id" {
+  value = module.elasticcache.elasticcache-1-cluster-id
 }
