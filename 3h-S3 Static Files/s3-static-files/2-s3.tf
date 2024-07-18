ef460f521e16ab948befe403404a1027543e5627
BrunoFerreira10 <brunoferreira86dev@gmail.com>
2024-07-18 16:29:16 -0300

    Fix

--- 3h-S3 Static Files/s3-static-files/2-s3.tf
+++ 3h-S3 Static Files/s3-static-files/2-s3.tf
@@ -44,3 +44,46 @@ resource "aws_s3_bucket_policy" "magento-static-files-policy" {
   })
 }
 
+resource "aws_iam_role" "magento-s3-role" {
+  name = "magento-s3-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_policy" "magento-s3-policy" {
+  name = "magento-s3-policy"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "s3:PutObject",
+          "s3:GetObject",
+          "s3:ListBucket"
+        ]
+        Effect = "Allow"
+        Resource = [
+          "${aws_s3_bucket.magento-static-files.arn}",
+          "${aws_s3_bucket.magento-static-files.arn}/*"
+        ]
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "magento-s3-attachment" {
+  role       = aws_iam_role.magento-s3-role.name
+  policy_arn = aws_iam_policy.magento-s3-policy.arn
+}
