fac97ae62fbc277d742c300755cb6c0339c3d687
BrunoFerreira10 <brunoferreira86dev@gmail.com>
2024-07-23 17:42:33 -0300

    Open search update

--- 3f-Open Search/opensearch/1-opensearch.tf
+++ 3f-Open Search/opensearch/1-opensearch.tf
@@ -4,10 +4,11 @@ resource "aws_opensearch_domain" "opensearch-1" {
   domain_name = "opensearch-${var.shortnameid}-1"
   
   cluster_config {
-    instance_type = "t3.small.search"
+    # instance_type = "t3.small.search"
+    instance_type = "t3.medium.search"
     instance_count = 2
     dedicated_master_enabled = true
-    dedicated_master_type = "t3.small.search"
+    dedicated_master_type = "t3.medium.search"
     dedicated_master_count = 3
     zone_awareness_enabled = true
   }
@@ -21,7 +22,8 @@ resource "aws_opensearch_domain" "opensearch-1" {
   vpc_options {
     subnet_ids = [
       data.terraform_remote_state.remote-state-vpc.outputs.vpcs-subnet-vpc-1-private-1a-id,
-      data.terraform_remote_state.remote-state-vpc.outputs.vpcs-subnet-vpc-1-private-1b-id      
+      data.terraform_remote_state.remote-state-vpc.outputs.vpcs-subnet-vpc-1-private-1b-id,
+      data.terraform_remote_state.remote-state-vpc.outputs.vpcs-subnet-vpc-1-private-1c-id
     ]
     security_group_ids = [
       data.terraform_remote_state.remote-state-vpc.outputs.vpcs-sg-vpc-1-opensearch-1-id
