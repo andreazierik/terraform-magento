53efa9620e462999ef2cf322ea5df65a41d22b05
BrunoFerreira10 <brunoferreira86dev@gmail.com>
2024-07-24 18:52:53 -0300

    Fix

--- 3c-EFS/efs/efs.tf
+++ 3c-EFS/efs/efs.tf
@@ -25,6 +25,14 @@ resource "aws_efs_mount_target" "efs-1-mntpnt-subnet-private-1b" {
   ]
 }
 
+resource "aws_efs_mount_target" "efs-1-mntpnt-subnet-private-1c" {
+  file_system_id = aws_efs_file_system.efs-1.id
+  subnet_id      = data.terraform_remote_state.remote-state-vpc.outputs.vpcs-subnet-vpc-1-private-1c-id
+  security_groups = [
+    data.terraform_remote_state.remote-state-vpc.outputs.vpcs-sg-vpc-1-efs-1-mountpoints-id
+  ]
+}
+
 resource "aws_efs_backup_policy" "efs-1-backup-policy" {
   file_system_id = aws_efs_file_system.efs-1.id
 
