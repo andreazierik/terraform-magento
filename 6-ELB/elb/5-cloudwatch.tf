7e2294641a1e639f2dbc52035ee4d8eb003aa3e9
BrunoFerreira10 <brunoferreira86dev@gmail.com>
2024-07-24 10:39:40 -0300

    Fix

--- 6-ELB/elb/5-cloudwatch.tf
+++ 6-ELB/elb/5-cloudwatch.tf
@@ -1,10 +1,10 @@
 resource "aws_cloudwatch_metric_alarm" "alarm-cpu-bigger-than-limit" {
   alarm_name                = "cpu-bigger-than-limit"
   comparison_operator       = "GreaterThanOrEqualToThreshold"
-  evaluation_periods        = 2
+  evaluation_periods        = 3
   metric_name               = "CPUUtilization"
   namespace                 = "AWS/EC2"
-  period                    = 20
+  period                    = 10
   statistic                 = "Average"
   threshold                 = 70
   alarm_description         = "Uso da CPU maior que o limite."
