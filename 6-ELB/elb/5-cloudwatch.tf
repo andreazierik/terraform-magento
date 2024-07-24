70f5ccac057a43889dfd5236d8cac44ca49d0e55
BrunoFerreira10 <brunoferreira86dev@gmail.com>
2024-07-24 12:53:07 -0300

    Fix

--- 6-ELB/elb/5-cloudwatch.tf
+++ 6-ELB/elb/5-cloudwatch.tf
@@ -40,5 +40,46 @@ resource "aws_cloudwatch_metric_alarm" "alarm-cpu-lower-than-limit" {
   alarm_actions = [
     aws_autoscaling_policy.cpu-limit-remove-instance.arn
   ]
-  
+}
+
+resource "aws_cloudwatch_dashboard" "dashboard-1" {
+  dashboard_name = "Magento Dashboard"
+
+  dashboard_body = jsonencode({
+    widgets = [
+      {
+        type   = "metric"
+        x      = 0
+        y      = 3
+        width  = 12
+        height = 6
+
+        properties = {
+          metrics = [
+            [
+              "AWS/EC2",
+              "CPUUtilization",
+              "AutoScalingGroupName",
+              aws_autoscaling_group.asg-alb-1.name
+            ]
+          ]
+          period = 300
+          stat   = "Average"
+          region = "us-east-1"
+          title  = "Auto Scaling Group - CPU Utilization"
+        }
+      },
+      {
+        type   = "text"
+        x      = 0
+        y      = 0
+        width  = 3
+        height = 3
+
+        properties = {
+          markdown = "Hello world"
+        }
+      }
+    ]
+  })
 }
