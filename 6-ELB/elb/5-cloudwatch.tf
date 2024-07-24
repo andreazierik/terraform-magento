398b32b63df463efd2a8029f2f94bdfb2e734364
BrunoFerreira10 <brunoferreira86dev@gmail.com>
2024-07-24 09:45:00 -0300

    Fix

---
+++ 6-ELB/elb/5-cloudwatch.tf
@@ -0,0 +1,25 @@
+resource "aws_cloudwatch_metric_alarm" "alarm-cpu-bigger-than-limit" {
+  alarm_name                = "cpu-bigger-than-limit"
+  comparison_operator       = "GreaterThanOrEqualToThreshold"
+  evaluation_periods        = 2
+  metric_name               = "CPUUtilization"
+  namespace                 = "AWS/EC2"
+  period                    = 120
+  statistic                 = "Average"
+  threshold                 = 70
+  alarm_description         = "Uso da CPU maior que o limite."
+  insufficient_data_actions = []
+}
+
+resource "aws_cloudwatch_metric_alarm" "alarm-cpu-lower-than-limit" {
+  alarm_name                = "cpu-lower-than-limit"
+  comparison_operator       = "LessThanOrEqualToThreshold"
+  evaluation_periods        = 2
+  metric_name               = "CPUUtilization"
+  namespace                 = "AWS/EC2"
+  period                    = 120
+  statistic                 = "Average"
+  threshold                 = 30
+  alarm_description         = "Uso da CPU menor que o limite."
+  insufficient_data_actions = []
+}
