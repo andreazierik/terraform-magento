9d0abc90e41e8aaca6f21a28e91b314e8ed3826b
BrunoFerreira10 <brunoferreira86dev@gmail.com>
2024-07-25 10:08:28 -0300

    Fix

--- 4a-EC2 Setup/ec2-setup/1-iam.tf
+++ 4a-EC2 Setup/ec2-setup/1-iam.tf
@@ -2,4 +2,8 @@ resource "aws_iam_instance_profile" "magento-instance-profile" {
 
   name = "magento-instance-profile"
   role = data.terraform_remote_state.remote-state-s3-static-files.outputs.s3-static-files-magento-s3-role-name
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
