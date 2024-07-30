3d3c4d3d1c81cdb7645815df14a2e32cfccee92b
Erik Andreazi <andreazi.erik@gmail.com>
2024-07-29 22:26:26 -0300

    Comentando remote-ami

--- 6-ELB/elb/main.tf
+++ 6-ELB/elb/main.tf
@@ -7,15 +7,15 @@ data "terraform_remote_state" "remote-state-vpc" {
   }
 }
 
-data "terraform_remote_state" "remote-ami" {
-  backend = "s3"
-  config = {
-    region = var.regiao
-    bucket = var.remote-state-bucket
-    key    = "AMI Create/terraform.tfstate"
-    # key    = "AMI Update/terraform.tfstate"
-  }
-}
+# data "terraform_remote_state" "remote-ami" {
+#   backend = "s3"
+#   config = {
+#     region = var.regiao
+#     bucket = var.remote-state-bucket
+#     key    = "AMI Create/terraform.tfstate"
+#     # key    = "AMI Update/terraform.tfstate"
+#   }
+# }
 
 data "terraform_remote_state" "remote-ssl-certificate" {
   backend = "s3"
